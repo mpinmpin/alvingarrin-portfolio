c75da820cf86fe7907657ff4400d398a8ad8d90d
mpinoh <alvingarrin@gmail.com>
2024-05-09 10:17:38 +0700

    3rd Branch (responsive navbar)

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,11 +4,55 @@ import ThemeToggler from './ThemeToggler';
 
 const Navbar = () => {
 
+    const showSidebar = () => {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.display = 'flex';
+      };
+      
+    const hideSidebar = () => {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.display = 'none';
+      };
+
     return ( 
-        <nav className="navbar">
+        <nav>
+            <ul className="navbar">
             <div className="page-title">
                 <Link href="/"><h1>alvingarrin</h1></Link>
             </div>           
+            <div className="links">
+                <div className="dropdown hideOnMobile">
+                    <button className="dropbtn">Simple Apps
+                    </button>
+                    <div className="dropdown-content">
+                        <Link href="/weatherapp">Weather App</Link>
+                        <Link href="/calculator">Calculator</Link>
+                        <Link href="/bmicalculator">BMI Calculator</Link>
+                        {/* <Link href="/counter">Simple Counter</Link> */}
+                    </div>
+                </div>
+                <div className="dropdown hideOnMobile">
+                    <button className="dropbtn">Blogs
+                    </button>
+                    <div className="dropdown-content">
+                        <Link href="/blogs">Blogs Page</Link>
+                        <Link href="/create">Create New Blog</Link>
+                    </div>
+                </div>
+                <div className="dropdown  hideOnMobile">
+                    <Link href="/about">About</Link>
+                </div>
+                <div className="dropdown">
+                    {/* <ThemeToggler /> */}
+                </div>
+                <button className='sidebar-show' onclick={showSidebar}>show sidebar
+                </button>   
+            </div>
+            </ul>
+
+            <ul className="sidebar">
+                <button className='sidebar-hide' onclick={hideSidebar}>close sidebar
+                </button>         
             <div className="links">
                 <div className="dropdown">
                     <button className="dropbtn">Simple Apps
@@ -35,7 +79,9 @@ const Navbar = () => {
                     {/* <ThemeToggler /> */}
                 </div>
             </div>
+            </ul>
         </nav>
+        
      );
 }
  
--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ const About = () => {
                 <p>I&lsquo;m a web developer (and programming) enthusiast for over 18 months, working with JavaScript, HTML5, CSS3, React, and Next. I have built simple apps here like weather app, blog pages and BMI calculator. I might work with larger projects in the near future.</p>
                 <p>I also have strong interest in Data Science. I use Python and Scikit-learn in this regard.</p>
                 <p>Aside from my recent interest in programming, I&lsquo;ve been freelancing as math/physics/chemistry private tutor for mid-high school students for over 9 years.</p>
-                <p>Graduated in 2014, I also hold Bachelor of Engineering in Materials Engineering.</p>
+                <p>Graduated in 2014, I also hold Bachelor of Engineering in Materials Engineering from Bandung Institute of Technology.</p>
             </div>
         </>
      );
